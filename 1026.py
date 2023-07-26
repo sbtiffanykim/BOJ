a89c4c6d1d916f9c1115dbd72a9b5432bedde176
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2023-07-26 11:42:31 +0900

    solve the #1026

--- 1026.py
+++ 1026.py
@@ -1,9 +1,13 @@
 n = int(input())
 arr1 = sorted(list(map(int, input().split())))
-arr2 = sorted(list(map(int, input().split())), reverse=True)
+arr2 = list(map(int, input().split()))
 
 ans = 0
+
+# arr1 작은 수 * arr2 큰 수
 for k in range(n):
-    ans += arr1[k] * arr2[k]
+    max_val = max(arr2)
+    arr2.pop(arr2.index(max_val))
+    ans += arr1[k] * max_val
 
 print(ans)
