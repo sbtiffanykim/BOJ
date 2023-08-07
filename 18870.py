dc759191bb95e8a8d77e7900510df479fab7383b
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2023-08-07 12:28:56 +0900

    solve the #18870 using set

--- 18870.py
+++ 18870.py
@@ -4,14 +4,7 @@ input = sys.stdin.readline
 
 n = int(input())
 nums = list(map(int, input().split()))
-ordered_nums = sorted(nums)
-# 중복 제거 위한 임시배열
-temp = [ordered_nums[0]]
-
-# 중복된 숫자 제거한 배열
-for i in range(1, len(ordered_nums)):
-    if ordered_nums[i] != ordered_nums[i-1]:
-        temp.append(ordered_nums[i])
+temp = sorted(list(set(nums)))
 
 # nums돌면서 해당 숫자가 temp에서 이분탐색한 결과 반환
 for n in nums:
